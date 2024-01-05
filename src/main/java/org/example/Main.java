b1f4d1775570865a5d11c979a771d02d5a2fc3f1
Yana Vezhnovets <yana.vezhnovec@gmail.com>
2024-01-05 15:36:44 +0300

    о наболевшем

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("жаба залупа какая-то!!");
+        System.out.println("хочу на проект");
 
         }
     }
